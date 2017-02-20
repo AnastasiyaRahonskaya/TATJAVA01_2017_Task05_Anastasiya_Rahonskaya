7e79f0ae6d2601ccc77f8fd53229aa832bf8e3bb
Nastya <rahonskaya.n@gmail.com>
2017-02-20 21:07:56 +0300

    add parsers

--- src/main/java/by/epam/bean/Dish.java
+++ src/main/java/by/epam/bean/Dish.java
@@ -15,6 +15,16 @@ public class Dish implements Serializable {
   private String portion;
   private String price;
 
+  public Dish(int id, String category, String photo, String name, String description, String portion, String price) {
+    this.id = id;
+    this.category = category;
+    this.photo = photo;
+    this.name = name;
+    this.description = description;
+    this.portion = portion;
+    this.price = price;
+  }
+
   public Dish() {
   }
 
@@ -72,5 +82,34 @@ public class Dish implements Serializable {
 
   public void setPrice(String price) {
     this.price = price;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Dish dish = (Dish) o;
+
+    if (getId() != dish.getId()) return false;
+    if (getCategory() != null ? !getCategory().equals(dish.getCategory()) : dish.getCategory() != null) return false;
+    if (getPhoto() != null ? !getPhoto().equals(dish.getPhoto()) : dish.getPhoto() != null) return false;
+    if (getName() != null ? !getName().equals(dish.getName()) : dish.getName() != null) return false;
+    if (getDescription() != null ? !getDescription().equals(dish.getDescription()) : dish.getDescription() != null)
+      return false;
+    if (getPortion() != null ? !getPortion().equals(dish.getPortion()) : dish.getPortion() != null) return false;
+    return getPrice() != null ? getPrice().equals(dish.getPrice()) : dish.getPrice() == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = getId();
+    result = 31 * result + (getCategory() != null ? getCategory().hashCode() : 0);
+    result = 31 * result + (getPhoto() != null ? getPhoto().hashCode() : 0);
+    result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+    result = 31 * result + (getDescription() != null ? getDescription().hashCode() : 0);
+    result = 31 * result + (getPortion() != null ? getPortion().hashCode() : 0);
+    result = 31 * result + (getPrice() != null ? getPrice().hashCode() : 0);
+    return result;
   }
 }
